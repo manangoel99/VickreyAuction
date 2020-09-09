cd79557e4e0d1592c082a753f18fcef3252c723c
Manan Goel <manan.goel@research.iiit.ac.in>
2020-09-09 10:27:35 +0530

    Added some information

--- contracts/BiddingRing.sol
+++ contracts/BiddingRing.sol
@@ -24,19 +24,29 @@ contract BiddingRing {
         numBidders++;
     }
 
+    function getNumBidders() public view returns(uint) {
+        return numBidders;
+    }
+
     address public highBidder = msg.sender;
     uint public nonceBid;
     uint public highBid;
 
     function reveal(uint amount, uint nonce) public returns (address) {
-        require(now >= endOfBidding && now < endOfRevealing, "Reveal not performed during reveal period");
+        // Following line must be uncommented
+        // This function should not return anything! It is just for checking
+        // require(now >= endOfBidding && now < endOfRevealing, "Reveal not performed during reveal period");
+        // bytes memory encoding = abi.encodePacked(amount, nonce);
+        // require(keccak256(abi.encode(amount)) == hashedBidOff[msg.sender]);
         require(keccak256(abi.encodePacked(amount, nonce)) == hashedBidOff[msg.sender], "This User has not made this bid");
         if (amount > highBid) {
             highBid = amount;
             highBidder = msg.sender;
             nonceBid = nonce;   
         }
+        return highBidder;
     }
+
 
     // function submitToAuction() public {
     //     require(now > endOfRevealing);
